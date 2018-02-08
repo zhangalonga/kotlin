db31a1dcf4964d7c7114a74c3a3caad5b9ed1d8e
Mikhael Bogdanov <mikhael.bogdanov@jetbrains.com>
2018-02-08 13:59:01 +0100

    ~temporary

--- compiler/ir/ir.tree/build.gradle.kts
+++ compiler/ir/ir.tree/build.gradle.kts
@@ -8,6 +8,8 @@ jvmTarget = "1.6"
 
 dependencies {
     compile(project(":compiler:util"))
+    compile(intellijDep())
+    compile(intellijDep()) { includeJars("asm-all") }
     compile(project(":compiler:frontend"))
 }
 
