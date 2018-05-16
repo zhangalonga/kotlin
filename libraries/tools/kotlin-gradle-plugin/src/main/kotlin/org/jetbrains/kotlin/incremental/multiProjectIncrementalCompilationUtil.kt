31a0ecb802697432038891ba5b24bdbd9d972d8b
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2018-05-16 23:45:24 +0300

    Kapt: Remove kapt1 support from Gradle and incremental compilation

--- libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/incremental/multiProjectIncrementalCompilationUtil.kt
+++ libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/incremental/multiProjectIncrementalCompilationUtil.kt
@@ -1,17 +1,6 @@
 /*
- * Copyright 2010-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2010-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license
+ * that can be found in the license/LICENSE.txt file.
  */
 
 package org.jetbrains.kotlin.incremental
@@ -31,7 +20,6 @@ internal fun configureMultiProjectIncrementalCompilation(
         project: Project,
         kotlinTask: KotlinCompile,
         javaTask: AbstractCompile,
-        kotlinAfterJavaTask: KotlinCompile?,
         artifactDifferenceRegistryProvider: ArtifactDifferenceRegistryProvider,
         artifactFile: File?
 ) {
@@ -66,7 +54,6 @@ internal fun configureMultiProjectIncrementalCompilation(
         return cannotPerformMultiProjectIC(reason = "unknown task outputs to java destination dir ${illegalTask.path} $(${illegalTask.javaClass})")
     }
 
-    val kotlinCompile = kotlinAfterJavaTask ?: kotlinTask
-    kotlinCompile.artifactDifferenceRegistryProvider = artifactDifferenceRegistryProvider
-    kotlinCompile.artifactFile = artifactFile
+    kotlinTask.artifactDifferenceRegistryProvider = artifactDifferenceRegistryProvider
+    kotlinTask.artifactFile = artifactFile
 }
