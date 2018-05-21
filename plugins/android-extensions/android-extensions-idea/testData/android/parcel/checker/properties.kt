f058a89c7e3baaa24979ba2042711386ee16cc86
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2018-05-22 01:54:01 +0300

    Parcelable: Check @IgnoredOnParcel annotation usage (#KT-24459)

--- plugins/android-extensions/android-extensions-idea/testData/android/parcel/checker/properties.kt
+++ plugins/android-extensions/android-extensions-idea/testData/android/parcel/checker/properties.kt
@@ -1,11 +1,11 @@
 package test
 
-import kotlinx.android.parcel.Parcelize
+import kotlinx.android.parcel.*
 import android.os.Parcelable
 
 @Parcelize
 class A(val firstName: String) : Parcelable {
-    val <warning descr="[PLUGIN_WARNING] Property would not be serialized into a 'Parcel'. Add '@IgnoredOnParcel' annotation to remove the warning">secondName</warning>: String = ""
+    val <warning descr="[PLUtestTransitiveImplementWithAndroidGIN_WARNING] Property would not be serialized into a 'Parcel'. Add '@IgnoredOnParcel' annotation to remove the warning">secondName</warning>: String = ""
 
     val <warning descr="[PLUGIN_WARNING] Property would not be serialized into a 'Parcel'. Add '@IgnoredOnParcel' annotation to remove the warning">delegated</warning> by lazy { "" }
 
@@ -17,4 +17,19 @@ class A(val firstName: String) : Parcelable {
     var customSetter: String
         get() = ""
         set(<warning descr="[UNUSED_PARAMETER] Parameter 'v' is never used">v</warning>) {}
+}
+
+@Parcelize
+class B(@IgnoredOnParcel val firstName: String) : Parcelable {
+    @IgnoredOnParcel
+    var a: String = ""
+
+    @field:IgnoredOnParcel
+    var b: String = ""
+
+    @get:IgnoredOnParcel
+    var c: String = ""
+
+    @set:IgnoredOnParcel
+    var d: String = ""
 }
