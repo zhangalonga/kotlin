25c8d7428bd586ea9774acaee197c4be94529204
Dmitry Savvinov <dsavvinov@gmail.com>
2018-06-21 12:57:36 +0300

    Deprecate unsound smartcast from loop condition on assigned vars
    ^KT-22379 Fixed

--- compiler/util/src/org/jetbrains/kotlin/config/LanguageVersionSettings.kt
+++ compiler/util/src/org/jetbrains/kotlin/config/LanguageVersionSettings.kt
@@ -82,6 +82,7 @@ enum class LanguageFeature(
     FunctionTypesWithBigArity(KOTLIN_1_3, sinceApiVersion = ApiVersion.KOTLIN_1_3),
     RestrictRetentionForExpressionAnnotations(KOTLIN_1_3, kind = BUG_FIX),
     NoConstantValueAttributeForNonConstVals(KOTLIN_1_3, kind = BUG_FIX),
+    ProhibitSmartcastsFromLoopConditionForLoopAssignedVariables(KOTLIN_1_3, kind = BUG_FIX),
 
     StrictJavaNullabilityAssertions(sinceVersion = null, defaultState = State.DISABLED),
     ProperIeee754Comparisons(sinceVersion = null, defaultState = State.DISABLED, kind = BUG_FIX),
