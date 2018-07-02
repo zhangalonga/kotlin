3b3eb303e6a7c93c6c039a081943ab2c0772d6ee
Svyatoslav Kuzmich <svyatoslav.kuzmich@jetbrains.com>
2018-07-02 20:14:53 +0300

    ~ test hashCode values

--- js/js.translator/testData/box/builtins/hashCode.kt
+++ js/js.translator/testData/box/builtins/hashCode.kt
@@ -8,12 +8,25 @@ class A {
 class B
 
 fun box(): String {
-    if (A().hashCode() != 42) return "Wrong hash"
-    B().hashCode()
-    js("\"\"").hashCode()
-    js("123").hashCode()
-    123.hashCode()
-    "123".hashCode()
-    (123 as Any).hashCode()
+    if (A().hashCode() != 42) return "Wrong hash 0"
+
+    val o1 = B();
+    if (o1.hashCode() != o1.hashCode()) return "Wrong hash 1"
+
+    val o2 = js("\"\"")
+    if (o2.hashCode() != o2.hashCode()) return "Wrong hash 2"
+
+    val o3 = js("123")
+    if (o3.hashCode() != o3.hashCode()) return "Wrong hash 3"
+
+    val o4 = 123
+    if (o4.hashCode() != o4.hashCode()) return "Wrong hash 4"
+
+    val o5 = "123"
+    if (o5.hashCode() != o5.hashCode()) return "Wrong hash 5"
+
+    val o6 = (123 as Any)
+    if (o6.hashCode() != o6.hashCode()) return "Wrong hash 6"
+
     return "OK"
 }
