f0c30a7f3261f4c762cf1ba29d887b2a07465fc4
Alexander Udalov <Alexander.Udalov@jetbrains.com>
2018-07-05 14:23:14 +0200

    Support JvmName on annotation constructor parameters
     #KT-25372 Fixed

--- compiler/backend/src/org/jetbrains/kotlin/codegen/state/KotlinTypeMapper.java
+++ compiler/backend/src/org/jetbrains/kotlin/codegen/state/KotlinTypeMapper.java
@@ -486,7 +486,13 @@ public class KotlinTypeMapper {
         sw.writeReturnType();
         mapType(descriptor.getType(), sw, TypeMappingMode.VALUE_FOR_ANNOTATION);
         sw.writeReturnTypeEnd();
-        return sw.makeJvmMethodSignature(descriptor.getName().asString());
+        return sw.makeJvmMethodSignature(mapAnnotationParameterName(descriptor));
+    }
+
+    @NotNull
+    public String mapAnnotationParameterName(@NotNull PropertyDescriptor descriptor) {
+        PropertyGetterDescriptor getter = descriptor.getGetter();
+        return getter != null ? mapFunctionName(getter) : descriptor.getName().asString();
     }
 
     @NotNull
