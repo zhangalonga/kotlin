898fe089ffa8adbd2d063eb9b34141067ba6becf
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2018-07-09 04:06:13 +0300

    ++ Debugger: Add a missing read action (EA-119592) Yan Zhulanow 02/07/2018, 15:27

--- idea/idea-jvm/src/org/jetbrains/kotlin/idea/debugger/evaluate/KotlinRuntimeTypeEvaluator.kt
+++ idea/idea-jvm/src/org/jetbrains/kotlin/idea/debugger/evaluate/KotlinRuntimeTypeEvaluator.kt
@@ -34,6 +34,7 @@ import com.intellij.psi.search.GlobalSearchScope
 import com.sun.jdi.ClassType
 import com.sun.jdi.Value
 import org.jetbrains.eval4j.jdi.asValue
+import org.jetbrains.kotlin.idea.util.application.runReadAction
 import org.jetbrains.kotlin.psi.KtExpression
 import org.jetbrains.kotlin.psi.KtPsiFactory
 import org.jetbrains.kotlin.types.KotlinType
