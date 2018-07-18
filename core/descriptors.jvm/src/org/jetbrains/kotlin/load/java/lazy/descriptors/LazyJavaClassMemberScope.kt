af34ab58d9cb2136879c08f097cb5d38774515c7
Denis Zharkov <denis.zharkov@jetbrains.com>
2018-07-18 13:05:11 +0300

    Use THashSet for sets that are assumed to be big
    Otherwise, a lot of memory is wasted on nodes instances
    
    (cherry picked from commit 4922f87)

--- core/descriptors.jvm/src/org/jetbrains/kotlin/load/java/lazy/descriptors/LazyJavaClassMemberScope.kt
+++ core/descriptors.jvm/src/org/jetbrains/kotlin/load/java/lazy/descriptors/LazyJavaClassMemberScope.kt
@@ -71,7 +71,7 @@ class LazyJavaClassMemberScope(
     override fun computeMemberIndex() = ClassDeclaredMemberIndex(jClass, { !it.isStatic })
 
     override fun computeFunctionNames(kindFilter: DescriptorKindFilter, nameFilter: ((Name) -> Boolean)?) =
-        ownerDescriptor.typeConstructor.supertypes.flatMapTo(HashSet()) {
+        ownerDescriptor.typeConstructor.supertypes.flatMapTo(THashSet()) {
             it.memberScope.getFunctionNames()
         }.apply {
             addAll(declaredMemberIndex().getMethodNames())
